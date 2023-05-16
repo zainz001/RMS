bdd8bf1b873c2d2c92e6af37328c1e33a7deb044
zainz001 <uhanif30@gmail.com>
2023-05-16 08:41:03 -0700

    signup

--- code.php
+++ code.php
@@ -2,49 +2,45 @@
 session_start();
 include('dbconnect.php');
 
+if (isset($_POST['signup'])) {
+    if ($_POST['password'] == $_POST['cpassword']) {
+        if (isset($_POST['name']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['contact']) && isset($_POST['city']) && isset($_POST['user_type'])) {
 
-if(isset($_POST['signup']))
-{
-   
-    $name =$_POST['name'];
-    $email =$_POST['email'];
-    $password =$_POST['password'];
-    $encpassword=md5($password);
-    $contact =$_POST['contact'];
-    $city =$_POST['city'];
-    $user_type =$_POST['user_type'];
-
-    $query = "INSERT INTO users(name ,email, password,  contact, city,user_type) VALUES (:name,:email,:password, :contact,:city,:user_type)";
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $contact = $_POST['contact'];
+            $city = $_POST['city'];
+            $user_type = $_POST['user_type'];
 
 
 
-    $query_run = $conn->prepare($query);
-
-    $data =[
-        
-        ':name'=>$name,
-        ':email'=>$email,
-        ':password'=>$password,
-
-        ':contact'=>$contact,
-
-        ':city'=>$city,
-        ':user_type'=>$user_type,
+            $query = "INSERT INTO users(name ,email, password,  contact, city,user_type) VALUES (:name,:email,:password, :contact,:city,:user_type)";
 
 
-    ];
-    $query_run->execute($data);
-if ($query_execute) {
- $_SESSION['Message']= "inserted successfully";
- header('location:signup.html');
- exit(0);   
-} else {
-    $_SESSION['Message']= "not inserted successfully";
- header('location:signup.html');
- exit(0);
+
+            $query_run = $conn->prepare($query);
+
+            $data = [
+
+                ':name' => $name,
+                ':email' => $email,
+                ':password' => $password,
+                ':contact' => $contact,
+                ':city' => $city,
+                ':user_type' => $user_type,
+
+
+            ];
+            $query_run->execute($data);
+        }
+    } else if ($_POST['password'] != $_POST['cpassword']) {
+        echo '<script>  alert("Match both the passwords");  </script>';
+        header('location:signup.php');
+    }
 }
 
 
-}
+
 
 ?>
