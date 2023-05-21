17427894fca258471bbc4851d44610403c421d0a
zainz001 <uhanif30@gmail.com>
2023-05-21 09:50:54 -0700

    updatenavbar

--- admin/addTicketAction.php
+++ admin/addTicketAction.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('dbconnect.php');
+include('../dbconnect.php');
 
 
 if (isset($_POST['add_ticket'])) {
@@ -12,37 +12,50 @@ if (isset($_POST['add_ticket'])) {
         && isset($_POST['departure_location'])
         && isset($_POST['arrival_location'])
     ) {
+
+
+
+        $train_name = $_POST['train_name'];
+        $departure_date = $_POST['departure_date'];
+        $departure_time = $_POST['departure_time'];
+        $arrival_time = $_POST['arrival_time'];
+        $departure_location = $_POST['departure_location'];
+        $arrival_location = $_POST['arrival_location'];
+
+
+
+        $query = "INSERT INTO `train_ticket`(`train_name`, `date_available`, `departure_time`, `arrival_time`,`departure_location`, `destination`) VALUES (:train_name,:departure_date,:departure_time, :arrival_time,:departure_location,:arrival_location)";
+
+
+        $query_run = $conn->prepare($query);
+
+        $data = [
+
+            ':train_name' => $train_name,
+            ':departure_date' => $departure_date,
+            ':departure_time' => $departure_time,
+            ':arrival_time' => $arrival_time,
+            ':departure_location' => $departure_location,
+            ':arrival_location' => $arrival_location,
+
+
+        ];
+        $query_run->execute($data);
+        echo 'inserted successfully';
         
+        ?>
 
-
-            $train_name = $_POST['train_name'];
-            $departure_date = $_POST['departure_date'];
-            $departure_time = $_POST['departure_time'];
-            $arrival_time = $_POST['arrival_time'];
-            $departure_location = $_POST['departure_location'];
-            $arrival_location = $_POST['arrival_location'];
-
-
-
-            $query = "INSERT INTO `train_ticket`(`train_name`, `date_available`, `departure_time`, `arrival_time`,`departure_location`, `destination`) VALUES (:train_name,:departure_date,:departure_time, :arrival_time,:departure_location,:arrival_location)";
-
-
-            $query_run = $conn->prepare($query);
-
-            $data = [
-
-                ':train_name' => $train_name,
-                ':departure_date' => $departure_date,
-                ':departure_time' => $departure_time,
-                ':arrival_time' => $arrival_time,
-                ':departure_location' => $departure_location,
-                ':arrival_location' => $arrival_location,
-
-
-            ];
-            $query_run->execute($data);
-        }
+     
+    <?php
     }
-        
 
-    
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>    
+</head>
+<body>
+    <p>add more tickets <button type="submit"> <a href="addTickets.php"> add</a></button> </p>
+</body>
+</html>
--- admin/admin.php
+++ admin/admin.php
@@ -12,10 +12,9 @@
 <nav>
   <ul>
     <li><a href="#">Home</a></li>
-    <li><a href="#">Trains</a></li>
-    <li><a href="#">Schedules</a></li>
-    <li><a href="#">Tickets</a></li>
-    <li><a href="#">About</a></li>
+    <li><a href="allBookings.php">Trains</a></li>
+    <li><a href="allpackages.php">Schedules</a></li>
+    <li><a href="addTickets.php">Tickets</a></li>
     <li><a href="#">Contact</a></li>
   </ul>
 </nav>
